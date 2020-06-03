1b7f12cdd011dda732cc91d27980244e5168423d
Utkarsh Patil <utkarsh2305@gmail.com>
2020-06-03 09:05:12 +0530

    Add security provisions for notifications.

--- endorsity/routing.py
+++ endorsity/routing.py
@@ -2,17 +2,19 @@ from django.urls import path
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from notifications.consumers import NotificationConsumer
 
 
 application = ProtocolTypeRouter({
-    'websocket':
+    'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
                 [
                     path('ws/notifications/', NotificationConsumer),
                 ]
             )
-        )
+        ),
+    ),
 })
